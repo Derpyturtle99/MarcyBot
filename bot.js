8ddd595e04977ddfff41f83cef4e697f903c158c
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2017-11-23 17:55:42 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./package.json");
 
+client.user.SetGame(`test`);
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
