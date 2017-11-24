4b7479d6cbb1126e84109cb99f4eac3a05b9c2fd
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2017-11-24 00:06:37 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,18 @@ const client = new Discord.Client();
 
 // Variables
 
+// flipcoin
 function doCoinFlip() {
     var coinAnswer = ['Heads', 'Tails'];
 
     return coinAnswer[Math.floor(Math.random()*coinAnswer.length)];
 }
+//dice
+function doRollDice() {
+    var diceAnswer = ['1', '2', '3', '4', '5', '6'];
+
+    return
+diceAnswer[Math.floor(Math.random()*diceAnswer.length)];
 
 // Commands
 
@@ -15,22 +22,30 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+// Ping Command
 client.on('message', message => {
     if (message.content === '?ping') {
     	message.reply('Pong!');
   	}
 });
 
+// Nuzzle Command
 client.on('message', message => {
     if (message.content === '?nuzzle') {
       message.reply('*nuzzles* UwU');
    }
 });
 
+// Coin Flip Command
 client.on('message', message => {
     if (message.content === '?flipcoin') {
         message.reply(doCoinFlip() + '!');
     }
 });
+
+// Dice Roll Command
+client.on('message', message => {
+    if (message.content === '?rolldie') {
+        message.reply(doDiceRoll() + '!');
 
 client.login(process.env.BOT_TOKEN);
