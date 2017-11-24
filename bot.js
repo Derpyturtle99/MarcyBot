36ee8b3b199a54ee1b145f0a6c99e5eba02f515d
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2017-11-24 00:07:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,5 +47,7 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === '?rolldie') {
         message.reply(doDiceRoll() + '!');
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
