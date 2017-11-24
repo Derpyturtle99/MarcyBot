464c99d3e408a91199ddf07c1797c758b04359b4
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2017-11-23 20:41:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,5 +12,14 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+if message.content === '?purge') {
+const deleteCount = parseInt(args[0], 10);
+if (!deleteCount || deleteCount < 2 || deleteCount > 255)
+return message.reply("Please do not purge any messages outside of the 2-255 range. Thank you.");
+const fetched = await message.channel.fetchMessages({count: deleteCount});
+.catch(error => message.reply(`Oops! It seems like I couldn't delete the messages for you because of: ${error}`));
+}
+});
 
 client.login(process.env.BOT_TOKEN);
