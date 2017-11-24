0f03d1cae32f4551ad95655484c8db6c363610b0
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2017-11-23 23:55:22 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '?flipcoin') {
-        message.reply(message, doCoinFlip() + '!');
+        message.reply(doCoinFlip() + '!');
     }
 });
 
