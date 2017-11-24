6c9a89551041caeb3a24c0301f83217b9a588370
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2017-11-24 00:47:05 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,18 +37,6 @@ client.on('message', message => {
     }
 });
 
-// Echo Command
-client.on('message', message => {
-if (message.content === '?echo') {
-  const [channelid, messageid, quotename, ...note] = args.splice(1);
-  // I also support "here" as a channelID using this:
-  const channel = channelid == "here" ? message.channel : client.channels.get(channelid);
-  // I do the same with message ID, which can be "last":
-  const message = messageid === "last" ? msg.channel.messages.last(2)[0] : await channel.messages.get(messageid);
-  // pretend for a second this is the rest of the function:
-  insertInDB(quotename, channel.id, message.id, note.join(" "));
-}
-});
         
 
 client.login(process.env.BOT_TOKEN);
