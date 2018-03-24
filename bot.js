4daa65b22b0527ac2ef4015c9b5d389708193ecf
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2018-03-24 17:26:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,26 @@ const client = new Discord.Client();
 
 // Variables / Functions
 
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
 // flipcoin
 function doCoinFlip() {
     var coinAnswer = ['Heads', 'Tails'];
 
     return coinAnswer[Math.floor(Math.random()*coinAnswer.length)];
+}
+
+// dice
+function doDice() {
+    var diceAnswer = ['1', '2', '3', '4', '5', '6'];
+
+    return diceAnswer[Math.floor(Math.random()*diceAnswer.length)];
 }
 
 // Commands
@@ -42,6 +57,11 @@ Permanent Invite:
 https://discord.gg/RnB4Wac`);
     }
 });
+// Dice
+client.on('message', message => {
+    if (message.content === '?dice') {
+        message.reply(doDice() + '!');
+    }
     
 // Coin Flip Command
 client.on('message', message => {
