bab80e502d591a6859c3e241fefa0ee794df1b50
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2018-03-25 16:20:53 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('ready', () => {
 // Ping Command
 client.on('message', message => {
     if (message.content === '!ping') {
-        const m = await message.channel.send("Ping?");
+        const m = async message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   	}
 });
