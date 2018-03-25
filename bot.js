474559cd8a0a9a050365781dbde4e70b1de6782e
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2018-03-24 22:45:25 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ client.on('message', message => {
 
 // Say Command
 client.on('message', message => {
-    if (message.content === 'say' {
+    if (message.content === '?say') {
         message.reply(sayMessage() + '!');
     }
 });
