736cd09bb630a3f6ba6ff9ea90e761cc5b3b7141
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2018-03-26 16:15:52 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,17 +27,9 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
     if (command === '!setgame') {
         const gameSet = args.join(" ");
-        if  (message.content === '!setgame reset') {
-            message.reply('Game has been reset!');
-            return
-            if (gameSet === null) {
-                message.reply('Please provide a game you would like me to play!')
-                    else client.user.setPresence({ game: { name: gameSet, type: 0} });
-                    message.reply('Game set to: **' + (gameSet) + '**');
-            }
-        }
+        client.user.setPresence({ game: { name: gameSet, type: 0} });
+        message.reply('Game set to: **' + (gameSet) + '**');
     }
-            
 });
 
 // Purge Command
