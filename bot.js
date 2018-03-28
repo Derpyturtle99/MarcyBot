9302b2023756b4f23b8f3ee3724468dd4be974ea
Derpyturtle99 <33365193+Derpyturtle99@users.noreply.github.com>
2018-03-28 10:27:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,8 +63,8 @@ client.on('message', message => {
 // Purge Command
     if (command === '!purge') {
         const deleteCount = parseInt(args[0], 10);
-        if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-        return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+        if(!deleteCount || deleteCount < 1 || deleteCount > 101)
+        return message.reply("Please provide a number between 1 and 100 for the number of messages to delete");
         (message.channel.bulkDelete(deleteCount + 1))
         .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     }
